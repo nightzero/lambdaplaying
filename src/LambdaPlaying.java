b797cd788c2c9a0a1cf3d21c0f02e08e8b3571b4
Mattias Borgkvist <mattias.borgkvist@ericsson.com>
2015-09-03 14:03:50 +0200

    Updated.

--- src/LambdaPlaying.java
+++ src/LambdaPlaying.java
@@ -3,15 +3,22 @@ import java.util.stream.Stream;
 
 
 public class LambdaPlaying {
-
-	public static void main(String[] args) {
+	
+	public void testReduce(){
 		int count = Stream.of(1,2,3,4).reduce(0, (acc, element) -> acc + element);
 		System.out.println(count);
 		
 		Optional<Integer> count2 = Stream.of(1,2,3,4,5).reduce((acc, element) -> acc + element);
 		count2.ifPresent((Integer i) -> System.out.println(i));
-		//The following is a short syntax for the above
+		//Can be written as
+		count2.ifPresent(i -> System.out.println(i));
+		//The following is syntactic sugar for the above
 		count2.ifPresent(System.out::println);
+	}
+
+	public static void main(String[] args) {
+		LambdaPlaying play = new LambdaPlaying();
+		play.testReduce();
 	}
 
 }
