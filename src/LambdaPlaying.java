8ee38251a7fd3d61ae2215f993a36ae81f8691b0
Mattias Borgkvist <mattias.borgkvist@ericsson.com>
2015-09-22 18:17:05 +0200

    updated

--- src/LambdaPlaying.java
+++ src/LambdaPlaying.java
@@ -1,8 +1,16 @@
 import java.util.Optional;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 
 public class LambdaPlaying {
+	
+	@FunctionalInterface
+	public interface MyFunctionalInterface {
+		public void myFunc(String x);
+		default void bla2(){}
+		static void xyz(){}
+	}
 	
 	public void testReduce(){
 		int count = Stream.of(1,2,3,4).reduce(0, (acc, element) -> acc + element);
@@ -15,10 +23,26 @@ public class LambdaPlaying {
 		//The following is syntactic sugar for the above
 		count2.ifPresent(System.out::println);
 	}
+	
+	public void testSupplier(){
+		this.passParamToSupplier(() -> "Hello: " + getParam());
+	}
+	
+	private void passParamToSupplier(Supplier<String> expr) {
+		//If the next line is commented out, getParam() is not called.
+		System.out.println(expr.get());
+	}
+	private String getParam(){
+		System.out.println("Laizy eval...");
+		return "Nisse";
+	}
+	
+	public void testMyFunctionalInterface(MyFunctionalInterface k){
+		k.myFunc("Hej");
+	}
 
 	public static void main(String[] args) {
 		LambdaPlaying play = new LambdaPlaying();
-		play.testReduce();
+		play.testSupplier();
 	}
-
 }
